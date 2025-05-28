7dfc8aa761663b4fbd69701563d197ba64e59209
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-05-28 11:48:21 +0100

    [update] projects page

--- src/modules/projects/pages.tsx
+++ src/modules/projects/pages.tsx
@@ -153,15 +153,25 @@ export const ProjectsPage = () => {
     //   </div>
     // </div>
     <div className="max-w-6xl mx-auto py-8 px-4 relative">
-      <h1 className="text-3xl font-bold text-blue-900 mb-4">All Projects</h1>
 
-      <form onSubmit={handleFilterSubmit} className="mb-6 flex flex-wrap gap-4">
+      <div className="text-center mb-10">
+       <h1 className="text-4xl font-bold text-blue-900">
+           Discover and Build Projects Easily
+        </h1>
+        <p className="text-gray-600 mt-2">
+          Browse a collection of creative, beginner-to-advanced project ideas.
+        </p>
+      </div>
+
+      <form onSubmit={handleFilterSubmit} 
+      className="mb-8 flex md:flex-wrap gap-4 md:justify-center w-full"
+        >
         <TextInput
           ignoreFormik
           type="text"
           name="title"
           value={filters.title as string}
-          onChange={handleFilterChange as any}
+          onChange={handleFilterChange as React.ChangeEvent<HTMLInputElement>}
           placeholder="Search By Project Title"
         />
         {/* <TextInput
