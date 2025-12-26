d3d2bbc4c83d999f1a41cd55883426f7a95c41e7
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-12-26 17:35:53 +0100

    Remove Layout component from MyProfile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { ProfilePage } from "@/modules/profile/page";
-import Layout from "@/modules/layout";
+
 
 const MyProfile = () => {
   return (
-    <Layout>
       <ProfilePage />
-    </Layout>
   );
 };
 
