4721c8a54a7115d5c2ab1f52affb2c2bf04b7566
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-12-26 17:34:35 +0100

    Remove Layout component from LearningPaths

--- src/pages/paths/index.tsx
+++ src/pages/paths/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { LearningPathsPage } from "@/modules/paths/page";
-import Layout from "@/modules/layout";
 
 const LearningPaths = () => {
   return (
-    <Layout>
+    <>
       <LearningPathsPage />
-    </Layout>
+    </>
   );
 };
 
