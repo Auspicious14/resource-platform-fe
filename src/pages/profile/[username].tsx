4e8d083436f13b97fcf8fea43b44f57fdaed10f5
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-12-26 17:36:24 +0100

    Remove Layout wrapper from UserProfile component
    Removed Layout component from UserProfile.

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { ProfilePage } from "@/modules/profile/page";
-import Layout from "@/modules/layout";
 
 const UserProfile = () => {
   const router = useRouter();
   const { username } = router.query;
 
   return (
-    <Layout>
       <ProfilePage username={username as string} />
-    </Layout>
   );
 };
 
