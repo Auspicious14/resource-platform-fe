ac978794cf14df3e6f11e16aa280e92f5b6af381
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-12-26 17:35:13 +0100

    Remove Layout component from SubmissionReview

--- src/pages/submissions/[id].tsx
+++ src/pages/submissions/[id].tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { SubmissionReviewPage } from "@/modules/submissions/review";
-import Layout from "@/modules/layout";
 
 const SubmissionReview = () => {
   return (
-    <Layout>
       <SubmissionReviewPage />
-    </Layout>
   );
 };
 
