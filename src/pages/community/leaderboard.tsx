f45695abfee342214309e1f306731e1205dbbb8d
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-12-26 17:37:20 +0100

    Remove Layout component from Leaderboard

--- src/pages/community/leaderboard.tsx
+++ src/pages/community/leaderboard.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { LeaderboardPage } from "@/modules/community/leaderboard";
-import Layout from "@/modules/layout";
+
 
 const Leaderboard = () => {
   return (
-    <Layout>
       <LeaderboardPage />
-    </Layout>
   );
 };
 
